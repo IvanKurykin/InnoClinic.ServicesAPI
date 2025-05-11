e6ad011e468feffc907ae949eb6183204c93a471
IvanKurikin <ivan.kurykin@innowise.com>
2025-05-11 20:02:02 +0300

    update validator

--- InnoClinic.ServicesAPI/Application/Validators/ServiceCategoryRequestDtoValidator.cs
+++ InnoClinic.ServicesAPI/Application/Validators/ServiceCategoryRequestDtoValidator.cs
@@ -11,7 +11,7 @@ public class ServiceCategoryRequestDtoValidator : AbstractValidator<ServiceCateg
             .NotEmpty().WithMessage("Name is required.")
             .MaximumLength(100).WithMessage("Name must not exceed 100 characters.");
 
-        RuleFor(x => x.TimeSlotSize)
+        RuleFor(x => x.TimeSlotDuration)
             .GreaterThan(0).WithMessage("TimeSlotSize must be greater than 0.")
             .LessThanOrEqualTo(1440).WithMessage("TimeSlotSize must not exceed 1440 minutes (24 hours).");
     }
